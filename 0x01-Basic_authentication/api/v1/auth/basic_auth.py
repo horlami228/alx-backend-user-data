5cbed86fdd4711c733e80a6bb78b02793229d89f
horlami228 <akintolaolamilekan51@gmail.com>
2024-02-26 23:15:44 +0100

    Add BasicAuth class for basic authentication

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+""" Module of Auth views"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """Defines the BasicAuth class"""
+
+    def __init__(self):
+        """Initializes the BasicAuth class"""
+        pass
