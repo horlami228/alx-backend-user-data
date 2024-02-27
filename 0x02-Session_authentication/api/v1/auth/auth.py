8a1a1e0ad827b9ed5d01ccec2715827da4856578
horlami228 <akintolaolamilekan51@gmail.com>
2024-02-27 19:08:55 +0100

    Add session_cookie method to Auth class

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -3,6 +3,7 @@
 """ Module of Auth views"""
 from flask import request
 from typing import List, TypeVar
+import os
 
 
 class Auth():
@@ -33,3 +34,13 @@ class Auth():
         """Current user"""
 
         return None
+
+    def session_cookie(self, request=None):
+        """Returns a cookie value from a request"""
+
+        if request is None:
+            return None
+
+        cookie_name = os.getenv("SESSION_NAME")
+
+        return request.cookies.get(cookie_name)
