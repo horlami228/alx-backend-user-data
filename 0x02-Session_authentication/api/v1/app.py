068b985d3163781d54067efdd12f069e59e30a0c
horlami228 <akintolaolamilekan51@gmail.com>
2024-02-28 20:40:20 +0100

    Update auth requirements in app.py

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -33,9 +33,11 @@ def before_request():
     else:
         if auth.require_auth(request.path, ['/api/v1/status/',
                                             '/api/v1/unauthorized/',
-                                            '/api/v1/forbidden/']):
+                                            '/api/v1/forbidden/',
+                                            '/api/v1/auth_session/login/']):
 
-            if auth.authorization_header(request) is None:
+            if auth.authorization_header(request) is None\
+                    and auth.session_cookie(request) is None:
                 abort(401)
 
             current_user = auth.current_user(request)
