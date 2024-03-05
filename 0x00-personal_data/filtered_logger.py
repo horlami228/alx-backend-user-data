645b35e1226358afd40403f76388975bd473433a
horlami228 <akintolaolamilekan51@gmail.com>
2024-03-05 13:12:21 +0100

    Refactor filter_datum function in filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
-"""This function filters logs"""
+"""This module is for logs"""
 import re
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, seperator: str) -> str:
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, seperator: str) -> str:
     """returns the log message obsfucated"""
     for field in fields:
         message = re.sub(rf"{field}=.*?{seperator}",
