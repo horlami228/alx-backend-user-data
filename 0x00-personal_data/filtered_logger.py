34478af999a9cac89f22d1ba833e37d010f22b7c
horlami228 <akintolaolamilekan51@gmail.com>
2024-03-05 13:16:19 +0100

    fixed

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -9,6 +9,6 @@ def filter_datum(fields: List[str], redaction: str,
                  message: str, seperator: str) -> str:
     """returns the log message obsfucated"""
     for field in fields:
-        message = re.sub(rf"{field}=.*?{seperator}",
-                         rf"{field}={redaction}{seperator}", message)
+        message = re.sub(f"{field}=.*?{seperator}",
+                         f"{field}={redaction}{seperator}", message)
     return message
