d3db836e34acbe1f4af267ab364912ad45728b5a
horlami <akintolaolamilekan51@gmail.com>
2024-03-13 13:34:57 +0100

    Add user registration endpoint

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,14 +1,29 @@
 #!/usr/bin/env python3
-from flask import Flask, jsonify
+from auth import Auth
+from flask import Flask, jsonify, request
 
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
+AUTH = Auth()
+
 
 @app.route("/")
 def home():
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST", "GET"])
+def register_user():
+    data = request.get_json()
+    try:
+        AUTH.register_user(email=data.get("email"),
+                           password=data.get("password"))
+        return jsonify({"email": data.get("email"),
+                        "message": "user created"}, 201)
+    except Exception:
+        return jsonify({"message": "email already registered"}, 400)
 
 
 if __name__ == "__main__":
--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -7,7 +7,6 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.exc import InvalidRequestError
-
 from user import Base, User
 
 
