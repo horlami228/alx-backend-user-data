2f78a8af2bc76679b834f641432beef30c9b3003
horlami <akintolaolamilekan51@gmail.com>
2024-03-13 23:53:53 +0100

    Refactor auth.py: Remove unused _generate_uuid method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -11,6 +11,11 @@ def _hash_password(password: str) -> bytes:
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
+def _generate_uuid() -> str:
+    """Generates a unique UUID"""
+    return str(uuid.uuid4())
+
+
 class Auth:
     """Auth class to interact with the authentication database.
     """
@@ -39,7 +44,3 @@ class Auth:
                                   user.hashed_password)
         except NoResultFound:
             return False
-
-    def _generate_uuid(self) -> str:
-        """Generate UUID"""
-        return str(uuid.uuid4())
