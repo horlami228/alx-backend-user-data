fa4c3efda0e23b116484d1c5684d860de6922110
horlami <akintolaolamilekan51@gmail.com>
2024-03-14 10:10:53 +0100

    Add destroy_session method to Auth class

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -66,3 +66,12 @@ class Auth:
             return user
         except NoResultFound:
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """Destroy a session"""
+        if user_id is None:
+            return None
+        try:
+            self._db.update_user(user_id, session_id=None)
+        except NoResultFound:
+            return None
